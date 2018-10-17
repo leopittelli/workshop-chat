130e32f8fbb936fe104b741fd9aecd1f04f27945
leopittelli <leo6987@gmail.com>
2018-10-16 22:53:28 -0300

    fix video message html

--- code/public/chat-frontend.js
+++ code/public/chat-frontend.js
@@ -27,7 +27,7 @@ const chat = (function () {
             case 'image':
                 return `<img id="image-${ message.time }" src="${ message.url }" /> <button class="message-button face" title="Detectar caras" onclick="shape.detect('image-${ message.time }')"></button>`;
             case 'video':
-                return `<video id="video-${ message.time }" src="${ message.url }" controls/> <button class="message-button pip" title="Picture in picture" onclick="pip.toggle('video-${ message.time }')"></button>`;
+                return `<video id="video-${ message.time }" src="${ message.url }" controls></video> <button class="message-button pip" title="Picture in picture" onclick="pip.toggle('video-${ message.time }')"></button>`;
             case 'audio':
                 return `<audio controls src="${ message.url }" />`;
             default:
